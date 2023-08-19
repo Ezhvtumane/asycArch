7e6a5d4c0c3fb78eb581f1775c8c5445fe56ff2f
Georgy <orlov.g.s@outlook.com>
2023-08-19 19:47:38 +0400

    basic commit to master cherrypick

---
+++ accounting/src/main/java/com/georgyorlov/accounting/Accounting.java
@@ -0,0 +1,12 @@
+package com.georgyorlov.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accounting {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Accounting.class, args);
+    }
+}
--- template/src/main/java/com/georgyorlov/service/kafka/KafkaListenerService.java
+++ template/src/main/java/com/georgyorlov/service/kafka/KafkaListenerService.java
@@ -1,14 +1,24 @@
 package com.georgyorlov.service.kafka;
 
+import com.georgyorlov.schema.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaListenerService {
 
     @KafkaListener(topics = "example", groupId = "group-id")
     public void listen(String message) {
         System.out.println("Received Messasge in group - group-id: " + message);
+    }
+
+
+    @KafkaListener(topics = "employee", groupId = "group_id")
+    public void consume(ConsumerRecord<String, Employee> record) {
+        log.info(String.format("Consumed message -> %s", record.value()));
     }
 
 }
